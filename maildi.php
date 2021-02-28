c1cfdd566f7d9f9f7fae811eb9dd53d93248b40a
danbao090 <60874708+danbao090@users.noreply.github.com>
2021-02-28 16:18:18 +0100

    4

--- maildi.php
+++ maildi.php
@@ -1,11 +1,42 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n Message: $message";
-$recipient = "sangdidoge1101@hotmil.com";
-$subject = "Get in touch";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='generic.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+$action=$_REQUEST['action'];
+if ($action=="") /** display the contact form */
+{
+?>
+<form action="" method="POST" enctype="multipart/form-data">
+  <div class="fields">
+    <div class="field half">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div class="field half">
+      <label for="email">Email</label>
+      <input type="text" name="email" id="email" />
+    </div>
+    <div class="field">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" rows="5"></textarea>
+    </div>
+  </div>
+  <ul class="actions">
+    <input type="submit" value="Send"><input type="reset" value="Clear">
+  </ul>
+<?php
+}
+else/* send the submitted data */
+}
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$message=$_REQUEST['message'];
+if (($name=="")||($email=="")||($message==""))
+{
+echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+}
+else
+$from="From: $name\r\nReturn-path: $email";
+$subject="Message sent using your contact form";
+mail("youremail@yoursite.com", $subject, $message, $from);
+echo "Email sent!";
+}
+}
 ?>
