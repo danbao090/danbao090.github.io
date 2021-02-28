91017558c28f1dcfe52c484f8c0a929b539e0e3a
danbao090 <60874708+danbao090@users.noreply.github.com>
2021-02-28 17:33:41 +0100

    4

--- maildi.php
+++ maildi.php
@@ -1,42 +1,33 @@
 <?php
-$action=$_REQUEST['action'];
-if ($action=="") /** display the contact form */
-{
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
-<form action="" method="POST" enctype="multipart/form-data">
-  <div class="fields">
-    <div class="field half">
-      <label for="name">Name</label>
-      <input type="text" name="name" id="name" />
-    </div>
-    <div class="field half">
-      <label for="email">Email</label>
-      <input type="text" name="email" id="email" />
-    </div>
-    <div class="field">
-      <label for="message">Message</label>
-      <textarea name="message" id="message" rows="5"></textarea>
-    </div>
-  </div>
-  <ul class="actions">
-    <input type="submit" value="Send"><input type="reset" value="Clear">
-  </ul>
-<?php
-}
-else/* send the submitted data */
-}
-$name=$_REQUEST['name'];
-$email=$_REQUEST['email'];
-$message=$_REQUEST['message'];
-if (($name=="")||($email=="")||($message==""))
-{
-echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-}
-else
-$from="From: $name\r\nReturn-path: $email";
-$subject="Message sent using your contact form";
-mail("youremail@yoursite.com", $subject, $message, $from);
-echo "Email sent!";
-}
-}
-?>
+
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
+
+<form action="" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
