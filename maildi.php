c1e663c10a233f97db41f82117ce0b6208f19434
danbao090 <60874708+danbao090@users.noreply.github.com>
2021-02-28 15:42:10 +0100

    Update maildi.php

--- maildi.php
+++ maildi.php
@@ -7,5 +7,5 @@ $recipient = "sangdidoge1101@hotmil.com";
 $subject = "Get in touch";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='generic.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
